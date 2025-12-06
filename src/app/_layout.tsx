d7c9cc93c6ceac4062d04884fdea3e212749c97e
Amine Takdenti <aminetakdenti4@gmail.com>
2025-12-06 17:37:38 +0100

    Refactor layout and enhance country search functionality with input and pagination

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <QueryClientProvider client={queryClient}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        />
+      </QueryClientProvider>
+    </SafeAreaProvider>
+  );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,129 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  Image,
+  Pressable,
+  RefreshControl,
+  Text,
+  View,
+} from "react-native";
+
+import { useCountries, useSearchCountries } from "@/src/api";
+import { Input, SafeScreen } from "@/src/components";
+import { usePagination } from "@/src/hooks";
+import { Icons } from "@/src/utils";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useDebounce } from "use-debounce";
 
 export default function Index() {
+  const [search, setSearch] = useState("");
+  const [debouncedSearch] = useDebounce(search, 500);
+
+  const insets = useSafeAreaInsets();
+
+  const all = useCountries();
+
+  const searchQuery = useSearchCountries(debouncedSearch);
+
+  const countries = debouncedSearch.length > 0 ? searchQuery.data : all.data;
+  const refetch =
+    debouncedSearch.length > 0 ? searchQuery.refetch : all.refetch;
+  const isRefetching =
+    debouncedSearch.length > 0 ? searchQuery.isRefetching : all.isRefetching;
+
+  const { paginated, loadMore, hasMore, isLoadingMore } = usePagination(
+    countries || [],
+    20,
+  );
+
+  const clearSearchValue = () => {
+    setSearch("");
+  };
+
+  if (all.isLoading) {
+    return (
+      <SafeScreen
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      >
+        <Text>Loading...</Text>
+      </SafeScreen>
+    );
+  }
+
+  if (all.isError) {
+    return (
+      <SafeScreen
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      >
+        <Text>{all.error?.message}</Text>
+      </SafeScreen>
+    );
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeScreen>
+      <View style={{ flex: 1, paddingHorizontal: 25 }}>
+        <Input
+          value={search}
+          onChangeText={setSearch}
+          placeholder="Search countries..."
+          leftView={
+            <View style={{ marginRight: 8 }}>
+              <Icons.SearchIcon width={20} height={20} />
+            </View>
+          }
+          rightView={
+            debouncedSearch.length > 0 && (
+              <Pressable style={{ marginLeft: 8 }} onPress={clearSearchValue}>
+                <Icons.ClearIcon width={20} height={20} />
+              </Pressable>
+            )
+          }
+        />
+
+        <FlatList
+          data={paginated}
+          keyExtractor={(item) => item.cca3}
+          onRefresh={refetch}
+          refreshing={isRefetching}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingBottom: insets.bottom,
+          }}
+          refreshControl={
+            <RefreshControl refreshing={isRefetching} onRefresh={refetch} />
+          }
+          renderItem={({ item }) => {
+            return (
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  marginVertical: 10,
+                  gap: 10,
+                }}
+              >
+                <Image
+                  source={{ uri: item.flags.png }}
+                  style={{ height: 40, width: 60 }}
+                />
+                <View>
+                  <Text>{item.name.common}</Text>
+                  <Text>{item.cca3}</Text>
+                </View>
+              </View>
+            );
+          }}
+          ListFooterComponent={
+            isLoadingMore ? (
+              <Text>Loading more...</Text>
+            ) : hasMore ? (
+              <Button title="Load More" onPress={loadMore} />
+            ) : null
+          }
+        />
+      </View>
+    </SafeScreen>
   );
 }
