11399c989f6bef1a221b3aadc1ca59388c9b37ff
Amine Takdenti <aminetakdenti4@gmail.com>
2025-12-07 08:14:39 +0100

    Add styles for layout and container components in styles.ts

---
+++ src/styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 25,
+  },
+  flex1: {
+    flex: 1,
+  },
+  center: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  layoutRow: {
+    flexDirection: "row",
+  },
+  layoutCol: {
+    flexDirection: "column",
+  },
+});
